33b399ce5f58ad84c1a4c64750ec3dd902bfa094
Tuğba Yazıcı <106247691+tugbayzc@users.noreply.github.com>
2022-11-20 13:20:13 +0300

    first commit

---
+++ PaparaThirdWeek/PaparaThirdWeek.Api/Controllers/CompanyController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PaparaThirdWeek.Domain.Entities;
+using PaparaThirdWeek.Services.Abstracts;
+using PaparaThirdWeek.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace PaparaThirdWeek.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ICompanyService companyService;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            this.companyService = companyService;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Company company)
+        {
+           
+            companyService.Add(company);
+            return Ok();
+        }
+        [HttpGet("Companies")]
+        public IActionResult Get() 
+        {
+            var result = companyService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Company company) 
+        {
+          companyService.Delete(company);
+            return Ok();
+
+        }
+
+        [HttpPut]
+        public IActionResult Put(Company company)
+        {
+            companyService.Update(company);
+            return Ok();
+        }
+
+       
+      
+    }
+}
